aaf9b17224b685ecbb05b92a7281cf570c975feb
bea <beata.piechowicz@student.uj.edu.pl>
2018-01-06 00:49:50 +0100

    Forms - beginning

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -7,6 +8,12 @@ from app import app
 def index():
     user = {'username': 'Dżolo'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
